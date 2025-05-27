8ca1009da1c711b30064fbac1b7147309ee05a1e
AGH <akaa.a@hotmail.fr>
2025-05-27 09:06:17 +0200

    Mise a jour automatique via script .bat

--- _old/detect_devices.py
+++
@@ -1,8 +0,0 @@
-import nidaqmx.system
-
-system = nidaqmx.system.System.local()
-
-print("Appareils NI-DAQmx détectés :")
-for device in system.devices:
-    print(f"- {device.name} (Type: {device.product_type})")
-    print(f"  Modules/Channels: {[chan.name for chan in device.ai_physical_chans]}")
